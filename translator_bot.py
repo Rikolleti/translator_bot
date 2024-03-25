a046dde2cc92773951ea0b7e9aad90ee9b8dd4f2
azmamedov <azmamedov@ozon.ru>
2024-03-25 14:47:44 +0300

    Added avar translation

--- translator_bot.py
+++ translator_bot.py
@@ -37,6 +37,29 @@ def translate_russian_to_lezgin(input_text, vocab_file='lezgin_translate.txt'):
     translated_phrase = ' '.join(translated_words)
     return translated_phrase
 
+def translate_russian_to_avar(input_text, vocab_file='avar_translate.txt'):
+    vocab = load_vocabulary(vocab_file)
+    input_text = input_text.replace(',', ' ')
+    words = input_text.split()
+    translated_words = []
+    i = 0
+    while i < len(words):
+        # Check for one, two, and three-word phrases
+        for phrase_length in range(4, 0, -1):
+            phrase = ' '.join(words[i:i+phrase_length])
+            if phrase.lower() in vocab:
+                translated_words.append(vocab[phrase.lower()])
+                i += phrase_length  # Skip the next words
+                break
+        else:
+            # Use original word if not found
+            original_word = words[i]
+            if original_word.lower() not in ('в', 'на'):  # Exclude the letter "в"
+                translated_words.append(vocab.get(original_word.lower(), original_word))
+            i += 1
+    translated_phrase = ' '.join(translated_words)
+    return translated_phrase
+
 # Dictionary to store user states
 user_states = {}
 
@@ -46,14 +69,16 @@ def handle_start(message):
     user_first_name = message.from_user.first_name
     welcome_message = f"Добрый день, {user_first_name}!\nВас приветствует бот ДагоЛинго-Переводчик. 📚\n\nВыберите язык на который вы хотите перевести русский текст:"
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add("Лезгинский", "Аварский (в разработке)", "Кумыкский (в разработке)", "Даргинский (в разработке)", "Лакский (в разработке)")
+    markup.add("Лезгинский", "Аварский", "Кумыкский (в разработке)", "Даргинский (в разработке)", "Лакский (в разработке)")
     bot.send_message(message.chat.id, welcome_message, reply_markup=markup)
 
-@bot.message_handler(func=lambda message: message.text in ["Лезгинский", "Аварский (в разработке)", "Кумыкский (в разработке)", "Даргинский (в разработке)", "Лакский (в разработке)"])
+@bot.message_handler(func=lambda message: message.text in ["Лезгинский", "Аварский", "Кумыкский (в разработке)", "Даргинский (в разработке)", "Лакский (в разработке)"])
 def handle_language_selection(message):
     selected_language = message.text
     user_states[message.chat.id] = selected_language
     if selected_language == "Лезгинский":
+        bot.send_message(message.chat.id, f"Вы выбрали {selected_language}.\nТеперь введите текст для перевода:")
+    elif selected_language == "Аварский":
         bot.send_message(message.chat.id, f"Вы выбрали {selected_language}.\nТеперь введите текст для перевода:")
     else:
         bot.send_message(message.chat.id, "В разработке")  # Output "В разработке" for unsupported languages
@@ -71,8 +96,13 @@ def handle_translation_input(message):
         user_states[message.chat.id] = "start"  # Reset user state to start
         return
 
-    lezgin_translation = translate_russian_to_lezgin(input_text)
-    bot.send_message(message.chat.id, f"Перевод: {lezgin_translation}")
+    if selected_language == "Лезгинский":
+        lezgin_translation = translate_russian_to_lezgin(input_text)
+        bot.send_message(message.chat.id, f"Перевод: {lezgin_translation}")
+
+    if selected_language == "Аварский":
+        avar_translation = translate_russian_to_avar(input_text)
+        bot.send_message(message.chat.id, f"Перевод: {avar_translation}")
 
 # Start the bot
 bot.polling()
